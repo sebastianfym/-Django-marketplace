05ae04db3d5015a0c546ae30564725e0fd39e8e3
Sergey <joker-1237@mail.ru>
2022-08-23 20:47:05 +0300

    stage1/create template header_and_footer.html/fix

--- app_marketplace/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppMarketplaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_marketplace'
--- app_marketplace/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from app_marketplace import views
-
-urlpatterns = [
-    url('', views.HomePageView.as_view(), name='home')
-]
