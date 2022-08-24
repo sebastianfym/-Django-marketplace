98587e3a3d90596efd77682ffa885e916bf40c67
Sergey <joker-1237@mail.ru>
2022-08-24 23:27:03 +0300

    stage2/added admin panel, element carousel and create model banner/init

--- banners/admin.py
+++ banners/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CarouselModel
 
-# Register your models here.
+
+@admin.register(CarouselModel)
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ['image', 'goods_for_banner', 'discounts_for_banner']
--- banners/models.py
+++ banners/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from goods.models import Goods
+from discounts.models import Promotion
 
-# Create your models here.
+
+class CarouselModel(models.Model):
+    goods_for_banner = models.ForeignKey(Goods, on_delete=models.CASCADE, related_name='goods_fk')
+    discounts_for_banner = models.ForeignKey(Promotion, on_delete=models.CASCADE, related_name='discounts_fk')
