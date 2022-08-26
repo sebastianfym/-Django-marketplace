4c24c84d26777aec008949175ecb66783b22a75f
bg-god <bg-god@mail.ru>
2022-08-26 19:33:58 +0300

    stage2/category/create

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Goods
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+class GoodsAdmin(admin.ModelAdmin):
+    list_display = ["title", 'activity']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Goods, GoodsAdmin)
