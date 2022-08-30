e286de70e9c4a96cff20769bf1b43e2bfdd93953
Антон Иванов <fduecn2508@gmail.com>
2022-08-30 14:36:01 +0000

    Update config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,22 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('main', MainPageView.as_view(), name='main_page' ),
-    # path('main/<str:seller>', InfoAboutSellerView.as_view(), name='info_about_seller'),
-    # path('main/discounts', DiscountsView.as_view(), name='discounts'),
-    # path('main/cart', CartView.as_view(), name='cart'),
-    # path('main/purchase', PurchaseView.as_view(), name='purchase'),
-    # path('main/payment', PaymentView.as_view(), name='payment'),
-    # path('main/account', AccountView.as_view(), name='account'),
-    # path('main/account/edit', EditAccountView.as_view(), name='account'),
-    # path('main/account/history/view', HistoryView.as_view(), name='history_view'),
-    # path('main/account/history/purchase', HistoryPurchaseView.as_view(), name='history_purchase'),
-    # path('main/login', MyLoginView.as_view(), name='login'),
-    # path('main/logout', MyLogoutView.as_view(), name='logout'),
-    # path('main/register/', UserRegisterView.as_view(), name='register'),
-    # path('main/catalog', CatalogView.as_view(), name='catalog'),
-    # path('main/comparison', Comparison.as_view(), name='comparison'),
-    # path('main/prodcuts/<int:id_product>', ProductView.as_view(), name='product'),
     path('', include('customers.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
