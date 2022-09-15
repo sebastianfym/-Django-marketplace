cb1a0478054ca432d39cfd2e8489d8e802139308
ahil78 <ahil7800@gmail.com>
2022-09-15 18:36:02 +0700

    progress_html

--- goods/views.py
+++ goods/views.py
@@ -23,7 +23,7 @@ class CategoryView(View):
 
 class Catalog(CatalogMixin, ListView):
     model = Goods
-    template_name = 'goods/test_catalog.html'
+    template_name = 'goods/catalog.html'
     paginate_by = 8
     
     def get_queryset(self):
