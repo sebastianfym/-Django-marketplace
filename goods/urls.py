955c43d7cce6c89874a61d58d41cf8228f62e90b
ahil78 <ahil7800@gmail.com>
2022-10-27 21:05:57 +0700

    made

--- goods/urls.py
+++ goods/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CategoryView, detail_goods_page, ShowDetailProduct, CompareView, AddProductToCompareView, Catalog, \
-    HistoryList, DeleteProductFromCompareView
+from .views import CategoryView, ShowDetailProduct, CompareView, AddProductToCompareView, Catalog, \
+    HistoryList, DeleteProductFromCompareView, create_goods_related_images
 
 urlpatterns = [
     path('catalog/', Catalog.as_view(), name='catalog'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('compare/', CompareView.as_view(), name='compare'),
     path("compare_add/<int:id>", AddProductToCompareView.as_view(), name="compare_add"),
     path("compare_delete/<int:id>", DeleteProductFromCompareView.as_view(), name="compare_delete"),
-    path("<int:pk>/", detail_goods_page, name='post'),
+    # path("<int:pk>/", detail_goods_page, name='post'),
     path('detail/<int:pk>/', ShowDetailProduct.as_view(), name='post'),
+    path('related', create_goods_related_images)
 ]
