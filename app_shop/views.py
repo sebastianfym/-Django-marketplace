3553c7ac1e978474ef83d60959bdee6631d83f41
ahil78 <ahil7800@gmail.com>
2022-11-03 17:59:40 +0700

    progress

--- app_shop/views.py
+++ app_shop/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, ListView
 from discounts.views import get_banners
+from goods.models import Goods
+from app_shop.services import get_random_limit_goods
 
 
 class Index(TemplateView):
     template_name = 'app_shop/index.html'
 
     def get_context_data(self, **kwargs):
-        # print(self.request.user)
         context = super().get_context_data()
-        # print(context)
         banners = get_banners()
         context.update({'banners': banners})
+        context.update({'limit_edition': get_random_limit_goods()})
+        context.update({'popular': })
         return context
+
+
+#class BlockView(ListView):
+#    template_name = 'app_shop/blocks.html'
+#    model = Goods
+#
+#    def get_context_data(self, *, object_list=None, **kwargs):
+#        context = super().get_context_data()
+#        context['limit_edition'] = get_random_limit_goods()
+#        return context
+
