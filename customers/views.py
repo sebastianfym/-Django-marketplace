ca56be054d1c740961f8c06218aa5132906b0a14
bg-god <bg-god@mail.ru>
2022-11-10 15:23:33 +0300

    stage4/refactor2.0/clear_cache__image_viewing_goods_in_account

--- customers/views.py
+++ customers/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.decorators.cache import cache_page
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+
+from config.settings import CACHES_TIME
 from customers.forms import RegistrationForm, AccountAuthenticationForm, ChangeUserData
 from customers.models import CustomerUser
 from django.contrib import messages
 from django.utils.translation import gettext as _
+from goods.models import ViewHistory, Image
+from orders.models import Order
 
 
 class UserRegisterFormView(CreateView):
@@ -25,25 +30,6 @@ class UserRegisterFormView(CreateView):
 class AccountAuthenticationView(LoginView):
     template_name = 'customers/login.html'
     success_url = reverse_lazy('index')
-    #form_class = AccountAuthenticationForm
-    #def get(self, request, *args, **kwargs):
-    #    if request.user.is_authenticated:
-    #        return redirect("account/")
-    #    form = AccountAuthenticationForm()
-    #    return render(request, "customers/login.html", context={"form": form})
-#
-    #def post(self, request, *args, **kwargs):
-    #    form = AccountAuthenticationForm(request.POST or None)
-    #    if form.is_valid():
-    #        email = form.cleaned_data.get("email")
-    #        password = form.cleaned_data.get("password")
-    #        user = authenticate(email=email, password=password)
-    #        login(request, user)
-    #        return redirect('/profile')
-    #    context = {
-    #        "form": form,
-    #    }
-    #    return render(request, "customers/login.html", context)
 
 
 class MyLogoutView(LogoutView):
@@ -54,7 +40,8 @@ class UserProfile(View):
     """
     Данный класс служит для получения профиля авторизированного юзера и даёт возможность изменить данные.
     """
-    
+
+    @cache_page(3600 * CACHES_TIME)
     def get(self, request):
         user = request.user
         form = ChangeUserData(instance=user)
@@ -91,8 +78,17 @@ class UserAccount(View):
     """
     Данный класс является представлением аккаунта пользователя со всеми его данными
     """
+
+    @cache_page(3600 * CACHES_TIME)
     def get(self, request):
         user = request.user
+        view_goods = ViewHistory.objects.filter(customer=self.request.user)[:3]
+        try:
+            last_order = Order.objects.filter(customer=self.request.user)[:-1]
+        except ValueError:
+            last_order = None
         return render(request, "customers/account.html", context={
-            'user': user
+            'user': user,
+            'view_goods': view_goods,
+            'last_order': last_order
         })
