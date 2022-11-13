ab7241b13525583247f18386fb2007374a693286
ahil78 <ahil7800@gmail.com>
2022-11-13 13:21:11 +0700

    index ready

--- app_shop/tasks.py
+++ app_shop/tasks.py
@@ -1,8 +1,7 @@
 from config.celery import app
-from app_shop.services import get_limited_edition_goods, limit_edition_goods
+from app_shop.services import get_limited_edition_goods
 
 
 @app.task
 def update_offer_day():
-    limit_edition_goods.pop(0)
-    limit_edition_goods.append(get_limited_edition_goods())
+    get_limited_edition_goods()
--- app_shop/views.py
+++ app_shop/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.core.cache import cache
+from django.views.generic import TemplateView
 from discounts.views import get_banners
-from goods.models import Goods
-from app_shop.services import limit_edition_goods, popular_goods, hot_offer
+from app_shop.services import popular_goods, hot_offer
 
 
 class Index(TemplateView):
+    """
+    Класс представление главной страницы. Блоки hot_offer и limit_edition берутся из кэша с соответствующими ключами.
+    Инстансы товаров, находящихся в этих блоках заносятся в кэш в функции get_limit_edition в модуле services.py
+    """
     template_name = 'app_shop/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         banners = get_banners()
-        context.update(limit_edition_goods[0])
+        context.update({'limit_edition': cache.get('limit_edition')})
+        context.update({'offer_day': cache.get('offer_day')})
         context.update(popular_goods)
         context.update(hot_offer)
         return context
