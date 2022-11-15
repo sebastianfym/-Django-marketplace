771b8aa1ff0a9e0093a1c87345be989f55452dcc
ahil78 <ahil7800@gmail.com>
2022-11-15 21:49:17 +0700

    make supercategory

---
+++ config/celery.py
@@ -0,0 +1,20 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('data_import')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'update_limit_deal': {
+        'task': 'app_shop.tasks.update_offer_day',
+        'schedule': crontab()
+    }
+}
+
+
