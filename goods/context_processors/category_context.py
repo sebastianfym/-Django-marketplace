ae3fc0cc241e7ab7284088dab17a863d5a4add24
ahil78 <ahil7800@gmail.com>
2022-11-16 14:23:50 +0700

    added category_level, url logo, fixture supercategory

--- goods/context_processors/category_context.py
+++ goods/context_processors/category_context.py
@@ -1,12 +1,12 @@
-from goods.models import Category
+from goods.models import SuperCategory
 
 
 def all_categories_context(request):
-    return {'category': Category.objects.all()}
+    return {'supercategory': SuperCategory.objects.all()}
 
 
-def mycompare(request):
-    if not request.session.get("compare"):
-        return {'compare': 0}
-    else:
-        return {'compare': len(request.session.get("compare"))}
+#def mycompare(request):
+#    if not request.session.get("compare"):
+#        return {'compare': 0}
+#    else:
+#        return {'compare': len(request.session.get("compare"))}
