cfdb936c8fb608647c60e4d8df0a71b262f3ec4e
Bogdan <117022747+sebastianfym@users.noreply.github.com>
2022-12-05 20:22:50 +0300

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from orders.views import UserHistoryOrder, CreateOrder, AddCard, AddSomeoneCard, add_order_for_payment
+from orders.views import UserHistoryOrder, CreateOrder, AddCard, AddSomeoneCard, add_order_for_payment, OrderDetail
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add_someone_card/<int:pk>/', AddSomeoneCard.as_view(), name='add_someone_card'),
     path('add_card/<int:order_id>/pay/', add_order_for_payment, name='payment'),
     path('add_someone_card/<int:order_id>/pay/', add_order_for_payment, name='payment'),
+    path('order_detail/<int:pk>/', OrderDetail.as_view(), name='order_detail'),
 ]
